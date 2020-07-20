1f067a53356b8c55f0cbfbe8daa4dea03d3ac22d
Demo-Liu <44599931+Demo-Liu@users.noreply.github.com>
2020-07-20 14:55:17 +0800

    Update Constants.java

--- ProxyDownload/src/main/java/com/demoliu/common/Constants.java
+++ ProxyDownload/src/main/java/com/demoliu/common/Constants.java
@@ -8,13 +8,23 @@ package com.demoliu.common;
  */
 public class Constants {
 
-    private final static String PROJECT_LAST_DIR = System.getProperty("user.dir");
+    private static String PROJECT_LAST_DIR;
+
+    static {
+        String userDir = System.getProperty("user.dir");
+        if(userDir.startsWith("C:")){
+            //如果是C盘,可能没有写入权限,将持久化配置写入用户目录
+            PROJECT_LAST_DIR = System.getProperty("user.home")+"/.pd";
+        }else{
+            PROJECT_LAST_DIR = userDir + "/runtime";
+        }
+    }
 
     //代理配置文件
-    public static String PROXY_HOST_PRO = PROJECT_LAST_DIR + "/runtime/proxy.properties";
+    public static String PROXY_HOST_PRO = PROJECT_LAST_DIR + "/proxy.properties";
 
     //全局配置文件
-    public static String CONFIG_PRO =  PROJECT_LAST_DIR + "/runtime/config.properties";
+    public static String CONFIG_PRO =  PROJECT_LAST_DIR + "/config.properties";
 
     //校验url正则
     public final static String URL = "(https?|ftp|file)://[-A-Za-z0-9+&@#/%()?=~_|!:,.;]+[-A-Za-z0-9+&@#/%()=~_|]";
